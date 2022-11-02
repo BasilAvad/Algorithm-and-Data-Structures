b993b81a0530b32c2f6450411514c36fa2e0834c
BASİL AVAD <basilavad9@gmail.com>
2022-11-02 21:42:43 +0300

    2022.11.02-21:42
    Update

---
+++ Algorithms_BTK/Apps/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructures;
+namespace Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new DataStructures.Array.Array<int>();
+            
+            arr.Add(24);
+            arr.Add(24);
+            Console.WriteLine(arr.Count +" Capacity  : "+arr.Capacity );
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms_BTK/DataStructures/Array/Array.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Array
+{
+    public class Array<T> : IEnumerable<T>, ICloneable
+    {
+        private T[] InnerList;
+        public int Count { get; private set; }
+        public int Capacity => InnerList.Length;
+        // Yapılandırıcı sınıf
+        public Array()
+        {
+            InnerList = new T[2];
+            Count = 0;
+        }
+        public void Add(T item)
+        {
+            if (InnerList.Length == Count)
+                DoubleArray();
+            InnerList[Count] = item;
+            Count++;
+        }
+
+        private void DoubleArray()
+        {
+            var temp = new  T[InnerList.Length * 2];
+            System.Array.Copy(InnerList, temp, InnerList.Length);
+            InnerList = temp;
+            //for (int i = 0; i < InnerList.Length; i++)
+            //{
+            //    temp[i] = InnerList[i];
+            //}
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
